3b18ae46c62cd564672adf2eb2a8e3ace6b165bc
Yasushi400 <chen20030223@outlook.jp>
2022-11-24 15:27:27 +0900

    add story.html/js/css

--- ISD_15/felling/story.js
+++
@@ -1,7 +0,0 @@
-$(document).on("textarea",".story",function(){
-    var that = $(this);
-    var inputW =$(that).val().offsetWidth;
-    if ( inputW >= 50){
-        $(that).css('width', inputW + 'px');
-    }
-});
